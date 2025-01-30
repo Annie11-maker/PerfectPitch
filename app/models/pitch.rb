4b64be4d36ae9900072d06182b31a8676436bfc9
Sanju Varkey <5578483+chief10sguru@users.noreply.github.com>
2025-01-30 15:17:28 +0000

    AI Integration Done

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -5,4 +5,13 @@ class Pitch < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+
+  def content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "We are creating a presentation called #{name} based on #{description}. Generate 3 bullet points for a slide outlining potential problems based on #{name} and #{description}"}]
+    })
+    return chatgpt_response["choices"][0]["message"]["content"]
+  end
 end
