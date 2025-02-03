f5e656b90edabe896f2b4b40432921e2ed056d13
quratulain shouket <quratulainshouket11@gmail.com>
2025-02-03 15:04:18 +0000

    share button working, ai commented

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -6,12 +6,23 @@ class Pitch < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
 
-  # def content
-  #  client = OpenAI::Client.new
-  #  chatgpt_response = client.chat(parameters: {
-  #    model: "gpt-4o-mini",
-  #    messages: [{ role: "user", content: "We are creating a presentation called #{name} based on #{description}. Generate 3 bullet points for a slide outlining potential problems based on #{name} and #{description}"}]
-  #  })
-  #  return chatgpt_response["choices"][0]["message"]["content"]
-  # end
+  def content
+    # client = OpenAI::Client.new
+    # chatgpt_response = client.chat(parameters: {
+    #   model: "gpt-4o-mini",
+    #   messages: [
+    #     { role: "user", content: "We are creating a presentation called #{name} based on #{description}. Generate 4 bullet points describing the potential problems/pain points for the second slide. Generate 4 bullet points describing the target audience for the third slide. Generate 4 bullet points describing the solution for the fourth slide. Do not include any introductory phrases like 'Here are the points'—just the bullet points." }
+    #   ]
+    # })
+    # return chatgpt_response["choices"][0]["message"]["content"]
+    "Pain point 1\nPain point 2\nPain point 3\nPain point 4\nAudience point 1\nAudience point 2\nAudience point 3\nAudience point 4\nSolution point 1\nSolution point 2\nSolution point 3\nSolution point 4"
+
+  end
+
+  def related_image
+    fetcher = ImageFetcher.new(name)
+    fetcher.fetch_image
+  end
+
+
 end
