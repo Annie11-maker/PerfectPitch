fafb5729f1223e248fd9f90d935550e1fa479b09
quratulain shouket <quratulainshouket11@gmail.com>
2025-02-05 09:51:44 +0000

    changes

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -8,15 +8,15 @@ class Pitch < ApplicationRecord
   validates :description, presence: true, length: { maximum: 500 }
 
   def content
-    # client = OpenAI::Client.new
-    # chatgpt_response = client.chat(parameters: {
-    #   model: "gpt-4o-mini",
-    #   messages: [
-    #     { role: "user", content: "We are creating a presentation called #{name} based on #{description}. Generate 4 bullet points describing the potential problems/pain points for the second slide. Generate 4 bullet points describing the target audience for the third slide. Generate 4 bullet points describing the solution for the fourth slide. Do not include any introductory phrases like 'Here are the points'—just the bullet points." }
-    #   ]
-    # })
-    # return chatgpt_response["choices"][0]["message"]["content"]
-    "Pain point 1\nPain point 2\nPain point 3\nPain point 4\nAudience point 1\nAudience point 2\nAudience point 3\nAudience point 4\nSolution point 1\nSolution point 2\nSolution point 3\nSolution point 4"
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "user", content: "We are creating a presentation called #{name} based on #{description}. Generate 4 bullet points describing the potential problems/pain points for the second slide. Generate 4 bullet points describing the target audience for the third slide. Generate 4 bullet points describing the solution for the fourth slide. Do not include any introductory phrases like 'Here are the points'—just the bullet points." }
+      ]
+    })
+    return chatgpt_response["choices"][0]["message"]["content"]
+    #"Pain point 1\nPain point 2\nPain point 3\nPain point 4\nAudience point 1\nAudience point 2\nAudience point 3\nAudience point 4\nSolution point 1\nSolution point 2\nSolution point 3\nSolution point 4"
 
   end
 
