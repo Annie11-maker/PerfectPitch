9ecd1255c84d0f5969b4cdce9c952ffa0265ad90
Gabriel Atak <machokatak@yahoo.com>
2025-02-06 16:09:44 +0000

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def render_404
+    redirect_to "/404"
+  end
 end
+
+
+# rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  # def record_not_found
+  #   render template: "errors/not_found", status: 404
+  # end
