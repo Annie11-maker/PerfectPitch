de7c0116e62c131152c5149fa81a8ba124055958
quratulain shouket <quratulainshouket11@gmail.com>
2025-07-07 13:45:10 +0100

    registration page updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   has_many :pitches
   has_many :favourites
   has_one_attached :photo
@@ -11,4 +8,7 @@ class User < ApplicationRecord
   validates :user_name, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
