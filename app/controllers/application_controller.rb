9a664f6fcfef1ee58690f18fbf8a87766df9cb07
quratulain shouket <quratulainshouket11@gmail.com>
2025-07-07 14:19:44 +0100

    updatecontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
 
   def render_404
     redirect_to "/404"
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :first_name, :last_name])
+  end
+
 end
 
 
